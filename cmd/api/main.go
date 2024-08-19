ceffb89298d2d949d402ec5518e443e20a30b8f3
Frank Diaz <dev.franklin.diaz@gmail.com>
2024-08-18 22:04:00 -0300

    Creando route

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
-
+    "github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"github.com/devfranklindiaz/notion-iol-integration/domain/service"
     "github.com/devfranklindiaz/notion-iol-integration/infrastructure/api"
@@ -16,9 +16,21 @@ func main() {
 	}
 	log.Println("Environment loaded")
 
-	notionService := service.NewNotionService()
-	notionHandler := api.NewNotionHandler(notionService)
+    notionService := service.NewNotionService()
+    log.Println("Notion service created")
 
-	http.HandleFunc("/connect", notionHandler.Connect)
-	log.Fatal(http.ListenAndServe(":8082", nil))
+    notionHandler := api.NewNotionHandler(notionService)
+    log.Println("Notion handler created")
+
+    r := mux.NewRouter()
+
+    apiRouter := r.PathPrefix("/api/v1").Subrouter()
+    apiRouter.HandleFunc("/connect", notionHandler.Connect).Methods(http.MethodPost)
+    log.Println("API router created")
+
+    err = http.ListenAndServe(":8081", r)
+
+    if err != nil {
+        log.Fatalf("Error starting server %v", err)
+    }
 }
